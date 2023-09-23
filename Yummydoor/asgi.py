872683d37b22fca1d3c37957fcc8e8b9695f026f
Shiv Kumar <shivec195@gmail.com>
2023-09-23 17:14:44 +0530

    initial code

---
+++ Yummydoor/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Yummydoor project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Yummydoor.settings')
+
+application = get_asgi_application()
